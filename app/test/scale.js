2347bdebd3213d50ec54f25fdc08c48cee855c75
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-05-05 06:36:32 +0200

    add expect but is not considered in test bundle

--- app/test/scale.js
+++ app/test/scale.js
@@ -1,5 +1,10 @@
-import Scale from "../js/vexflow/Scale2.js";
+import expect from "expect";
+//import Scale from "../js/vexflow/Scale2.js";
+
 
 Scale.registerScale({ name: "blues", notes: ["1", "b3", "4", "b5", "5", "b7"] });
 
 console.log(new Scale({ scale: "blues", root: "g" }));
+const s = new Scale({ scale: "blues", root: "g" });
+//expect(s.root).to.eql("g");
+//expect(s.scale).to.be.an("object");
