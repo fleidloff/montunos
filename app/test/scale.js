9439f41067ef38c236b1fb1488af59254d2f29df
Frederik Leidloff <fleidloff@team.mobile.de>
2016-05-10 14:32:39 +0200

    add missing semicolons to test

--- app/test/scale.js
+++ app/test/scale.js
@@ -2,7 +2,7 @@ import Scale from "../js/vexflow/Scale.js";
 
 export default function(test) {
     test("rootand key should be correct", (assert) => {
-        const s = new Scale({ root: "g", scale: "M" })
+        const s = new Scale({ root: "g", scale: "M" });
         
         assert.equal(s.root, "g");
         assert.equal(s.getKey(), "G");
@@ -10,22 +10,22 @@ export default function(test) {
     });
 
     test("notes must be correct for flat keys (b accidentals)", (assert) => {
-        const s = new Scale({ root: "g", scale: "m" })
+        const s = new Scale({ root: "g", scale: "m" });
         
         assert.equal(s.canonicalNote("b3"), "bb");
         assert.end();
     });    
 
     test("notes must be correct for sharp keys (# accidentals)", (assert) => {
-        const s = new Scale({ root: "g", scale: "M" })
+        const s = new Scale({ root: "g", scale: "M" });
         
         assert.equal(s.canonicalNote("b5"), "c#");
         assert.end();
     }); 
 
     test("notes must be correct for 2 scales at the same time", (assert) => {
-        const gm = new Scale({ root: "g", scale: "m" })
-        const g = new Scale({ root: "g", scale: "M" })
+        const gm = new Scale({ root: "g", scale: "m" });
+        const g = new Scale({ root: "g", scale: "M" });
         
         assert.equal(g.canonicalNote("b5"), "c#");
         assert.equal(gm.canonicalNote("b3"), "bb");
@@ -33,7 +33,7 @@ export default function(test) {
     }); 
 
     test("notes must be correct existing accidentals", (assert) => {
-        const s = new Scale({ root: "g", scale: "M" })
+        const s = new Scale({ root: "g", scale: "M" });
         
         assert.equal(s.note("M7"), "f");
         assert.equal(s.note("m7"), "fn");
@@ -42,7 +42,7 @@ export default function(test) {
     });  
 
     test("notes must be correct existing accidentals with bs", (assert) => {
-        const s = new Scale({ root: "g", scale: "m" })
+        const s = new Scale({ root: "g", scale: "m" });
         
         assert.equal(s.note("b3"), "b");
         assert.equal(s.note("M3"), "bn");
